30da5b44dcab4fabd9e17cffa4e2dc63e1e31557
maThiaslI152 <isaiah.pwnpk@gmail.com>
2025-03-23 15:46:18 +0700

    Set up Redis Celery and beat MongoDB PostgreDB

--- backend/myapp/tasks.py
+++
@@ -1,6 +0,0 @@
-from celery import shared_task
-
-@shared_task
-def add(x, y):
-    print(f"Task received arguments: x={x}, y={y}")  # Debugging
-    return x + y
--- backend/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import cached_data
-from .views import run_task
-
-urlpatterns = [
-    path('cache/', cached_data),
-    path('run-task/', run_task),
-]
